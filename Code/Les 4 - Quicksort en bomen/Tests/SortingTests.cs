6674879742047eb0aaf6052d6cd3c575e7b627b0
Eltjo Voorhoeve <eltjo@voorhoeve.demon.nl>
2020-09-17 22:46:04 +0200

    Removed secundary sorter tests

--- Code/Les 4 - Quicksort en bomen/Tests/SortingTests.cs
+++ Code/Les 4 - Quicksort en bomen/Tests/SortingTests.cs
@@ -10,7 +10,7 @@ namespace AD
     public class SortingTests
     {
         [Test, Combinatorial]
-        public void Sort2(
+        public void Sort(
                     [Values("InsertionSort", "MergeSort", "ShellSort", "QuickSort")] string sorterName,
                     [Values(0, 10, 300)] int n)
         {
@@ -21,37 +21,6 @@ namespace AD
 
             // Arrange
             sorter = DSBuilder.CreateSorter(sorterName);
-            Assert.IsNotNull(sorter != null);
-
-            // Arrange
-            for (int i = 0; i < n; i++)
-                list.Add(random.Next(0, 100000));
-            listCopy = new List<int>(list);
-            listCopy.Sort();
-
-            // Act
-            sorter.Sort(list);
-
-            // Assert
-            bool equal = list.SequenceEqual(listCopy);
-            Assert.IsTrue(equal);
-        }
-
-        [Test, Combinatorial]
-        public void Sort(
-                    [Values("QuickSort")] string sorterName,
-                    [Values(0, 10, 300)] int n)
-        {
-            List<int> list = new List<int>();
-            List<int> listCopy;
-            Sorter sorter;
-            System.Random random = new System.Random();
-
-            // Arrange
-            if (sorterName == "QuickSort")
-                sorter = DSBuilder.CreateQuickSorter();
-            else
-                sorter = null;
             Assert.IsNotNull(sorter != null);
 
             // Arrange
