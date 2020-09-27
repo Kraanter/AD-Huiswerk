ede3accf3f292a4b9e8aa995fe54c4906b61d0e4
Eltjo Voorhoeve <eltjo@voorhoeve.demon.nl>
2020-09-27 13:39:19 +0200

    Added more info on CheckBrackets

--- Code/Les 2 - Basis datastructuren/Huiswerk2/BracketChecker.cs
+++ Code/Les 2 - Basis datastructuren/Huiswerk2/BracketChecker.cs
@@ -5,11 +5,36 @@ namespace AD
     public static class BracketChecker
     {
 
+        /// <summary>
+        ///    Run over all characters in a string, push all '(' characters
+        ///    found on a stack. When ')' is found, it shoud match a '(' on
+        ///    the stack, which is then popped.
+        ///
+        ///    If ')' is found when no '(' is on the stack, this method will
+        ///    terminate prematurely, no further inspection is needed.
+        /// </summary>
+        /// <param name="s">The string to check</param>
+        /// <returns>Returns True if all '(' are matched by ')'.
+        /// Returns False otherwise.</returns>
         public static bool CheckBrackets(string s)
         {
             throw new System.NotImplementedException();
         }
 
+
+        /// <summary>
+        ///    Run over all characters in a string, when an opening bracket is
+		///    found the closing counterpart from closeChar is pushed on a Stack
+        ///    When an closing bracket is found, it should match the top character
+		///    from this stack.
+		///    
+        ///    This method will terminate prematurely if a wrong or missmatched
+        ///    closing bracket is found and no further inspection is needed.
+		/// </summary>
+		/// <param name="s">The string to check</param>
+		/// <returns>Returns True if all opening brackets are matched by
+		/// it's correct counterpart in a correct order.
+        /// Returns False otherwise.</returns>
         public static bool CheckBrackets2(string s)
         {
             throw new System.NotImplementedException();
