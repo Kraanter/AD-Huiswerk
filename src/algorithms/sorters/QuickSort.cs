219f30d5b3a4de744adf7a5dd15ab4a28b99a812
Kraanter <krijngrimme@hotmail.com>
2023-09-24 01:52:04 +0200

    Implemented QuickSort

--- src/algorithms/sorters/QuickSort.cs
+++ src/algorithms/sorters/QuickSort.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 
@@ -6,11 +7,66 @@ namespace AD
     public partial class QuickSort : Sorter
     {
         private static int CUTOFF = 3;
+        private static InsertionSort _insertionSort = new();
 
         public override void Sort(List<int> list)
         {
-            // Should be implemented in lecture 4!
-            throw new System.NotImplementedException();
+            Sort(list, 0, list.Count - 1);
+        }
+
+        private void Sort(List<int> list, int lo, int hi)
+        {
+            if (hi - lo <= CUTOFF)
+            {
+                _insertionSort.Sort(list, lo, hi);
+                return;
+            }
+            
+            int pivotIndex = GetPivotIndex(list, lo, hi);
+            int pivotNumber = list[pivotIndex];
+            // Swap the pivot with the end of the list
+            Swap(list, new (pivotIndex, hi));
+
+            int i = lo;
+            int j = hi - 1;
+
+            do
+            {
+                while (list[++i] < pivotNumber) ;
+                while (list[--j] > pivotNumber) ;
+                
+                Swap(list, new(i, j));
+                
+            } while (i <= j);
+            
+            // Sort everything less than pivot
+            Sort(list, lo, pivotIndex - 1);
+            // Sort everything more than pivot
+            Sort(list, pivotIndex + 1, hi);
+        }
+
+        private int GetPivotIndex(List<int> list, int lo, int hi)
+        {
+            int first = list[lo];
+            int middle = list[lo + (hi - lo) / 2];
+            int last = list[hi];
+
+            if ((first > middle && first < last) || (first > last && first < middle))
+                return lo;
+
+            if ((middle > first && middle < last) || (middle > last && middle < first))
+                return lo + (hi - lo) / 2;
+
+            return hi;
+        }
+
+        private void Swap(List<int> list, Tuple<int, int> positions)
+        {
+            int val1 = list[positions.Item1];
+            int val2 = list[positions.Item2];
+
+            list[positions.Item1] = val2;
+            list[positions.Item2] = val1;
         }
     }
 }
--- src/homework/Les 3 - Recursie en sorteren/Huiswerk3/Program.cs
+++ src/homework/Les 3 - Recursie en sorteren/Huiswerk3/Program.cs
@@ -6,32 +6,35 @@
         {
             System.Console.WriteLine("\n=====   Opgave 1 : Faculteit   =====\n");
             Opgave1.Run();
-
+            
             System.Console.WriteLine("\n=====   Opgave 2 : Fibonacci   =====\n");
             Opgave2.Run();
-
+            
             System.Console.WriteLine("\n=====   Opgave 3 : OmEnOm   =====\n");
             Opgave3.Run();
-
+            
             System.Console.WriteLine("\n=====   Opgave 4 : Enen   =====\n");
             Opgave4.Run();
-
+            
             System.Console.WriteLine("\n=====   Opgave 6 : ForwardString   =====\n");
             Opgave6.Run();
-
+            
             System.Console.WriteLine("\n=====   Opgave 7 : Sorting   =====\n");
             Sorter isort = new InsertionSort();
             Sorter msort = new MergeSort();
             Sorter ssort = new ShellSort();
+            Sorter qsort = new QuickSort();
             isort.Run();
             msort.Run();
             ssort.Run();
-            int[] numbers = {100, 1000, 10000};
+            qsort.Run();
+            int[] numbers = {100, 1000, 10000, 10000};
             foreach (int num in numbers)
             {
                 isort.RunWithTimer(num);
                 msort.RunWithTimer(num);
                 ssort.RunWithTimer(num);
+                qsort.RunWithTimer(num);
             }
         }
     }
