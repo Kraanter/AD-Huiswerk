f324af957865e49a11824ede0c50e1e10b61ea83
Kraanter <krijngrimme@hotmail.com>
2023-09-24 00:53:41 +0200

    Implemented ShellSort

--- src/algorithms/sorters/ShellSort.cs
+++ src/algorithms/sorters/ShellSort.cs
@@ -1,13 +1,54 @@
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 
 namespace AD
 {
     public partial class ShellSort : Sorter
     {
+        private static InsertionSort _insertionSort = new();
         public override void Sort(List<int> list)
         {
-            throw new System.NotImplementedException();
+            var increments = new List<int>();
+
+            int inc = (int) (list.Count / 2.2);
+            do
+            {
+                increments.Add(inc);
+                inc = (int)(inc / 2.2);
+            } while (inc > 1);
+
+            foreach (var increment in increments)    
+            {
+                Debug.WriteLine($"Increment: {increment}");
+                Sort(list, 0, increment);
+            }
+
+            new InsertionSort().Sort(list);
+        }
+        
+        private void Sort(List<int> list, int start, int increment)
+        {
+            if (start >= increment)
+                return;
+                    
+            List<int> numsToSort = new();
+
+            for (int i = start; i < list.Count; i += increment)
+            {
+                numsToSort.Add(list[i]);
+            }
+            
+            if (numsToSort.Count < 2)
+                return;
+            
+            _insertionSort.Sort(numsToSort);
+            
+            for (int i = start; i < list.Count; i += increment)
+                list[i] = numsToSort[i/increment];
+            
+            Sort(list, start + 1, increment);
         }
     }
 }
