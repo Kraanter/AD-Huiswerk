fbc7164dd479f72568fa93baf5f6f466a7a3dd9c
Kraanter <krijngrimme@hotmail.com>
2023-10-03 14:00:32 +0200

    Implemented PriorityQueue

--- src/datastructures/PriorityQueue/PriorityQueue.cs
+++ src/datastructures/PriorityQueue/PriorityQueue.cs
@@ -15,7 +15,8 @@ namespace AD
         //----------------------------------------------------------------------
         public PriorityQueue()
         {
-            throw new System.NotImplementedException();
+            array = new T[DEFAULT_CAPACITY + 1];
+            size = 0;
         }
 
         //----------------------------------------------------------------------
@@ -23,23 +24,79 @@ namespace AD
         //----------------------------------------------------------------------
         public int Size()
         {
-            throw new System.NotImplementedException();
+            return size;
         }
 
         public void Clear()
         {
-            throw new System.NotImplementedException();
+            array = new T[DEFAULT_CAPACITY + 1];
+            size = 0;
         }
 
         public void Add(T x)
         {
-            throw new System.NotImplementedException();
+            if (array.Length == ++size)
+            {
+                T[] newArray = new T[size * 2];
+                Array.Copy(array, newArray, size);
+                array = newArray;
+            }
+                
+            array[size] = x;
+            PercolateUp(size);
+        }
+
+        private void PercolateUp(int index)
+        {
+            if (index < 1)
+                return;
+            
+            if (array[index / 2].CompareTo(array[index]) > 0)
+            {
+                Swap(index, index/2);
+                PercolateUp(index/2);
+            }
+        }
+
+        private void PercolateDown(int index)
+        {
+            int lowestNrIndex = index;
+        
+            if (index * 2 <= size)
+                if (array[index * 2].CompareTo(array[lowestNrIndex]) < 0)
+                    lowestNrIndex = index * 2;
+            
+            if (index * 2 + 1 <= size)
+                if (array[index * 2 + 1].CompareTo(array[lowestNrIndex]) < 0)
+                    lowestNrIndex = index * 2 + 1;
+
+            if (lowestNrIndex == index)
+                return;
+            
+            Swap(index, lowestNrIndex);
+            PercolateDown(lowestNrIndex);
+        }
+
+        private void Swap(int index1, int index2)
+        {
+            if (index1 < 1 || index2 < 1)
+                return;
+            (array[index1], array[index2]) = (array[index2], array[index1]);
         }
 
         // Removes the smallest item in the priority queue
         public T Remove()
         {
-            throw new System.NotImplementedException();
+            if (size < 1)
+                throw new PriorityQueueEmptyException();
+
+            T returnItem = array[1];
+            array[1] = array[size];
+            array[size--] = default(T);
+            
+            PercolateDown(1);
+            
+            return returnItem;
         }
 
 
@@ -49,13 +106,30 @@ namespace AD
 
         public void AddFreely(T x)
         {
-            throw new System.NotImplementedException();
+            array[++size] = x;
         }
 
         public void BuildHeap()
         {
-            throw new System.NotImplementedException();
+            for (int i = size; i >= 1; i--)
+            {
+                PercolateDown(i);
+            }
         }
 
+        public override string ToString()
+        {
+            if (size < 1)
+                return "";
+            
+            string retString = array[1].ToString();
+
+            for (int i = 2; i <= size; i++)
+            {
+                retString += " " + array[i].ToString();
+            }
+
+            return retString;
+        }
     }
 }
